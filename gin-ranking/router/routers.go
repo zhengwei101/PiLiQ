3c00e4f39f4d384b7db2bbb1cb52b13e727e1ffd
wilbur-z <zhengwei101@aliyun.com>
2024-06-12 17:34:56 +0800

    [ADD] example for gin

---
+++ gin-ranking/router/routers.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router() *gin.Engine {
+	r := gin.Default()
+
+	user := r.Group("/user")
+	{
+		user.POST("/list", func(ctx *gin.Context) {
+			ctx.String(http.StatusOK, "user list")
+		})
+		user.PUT("/add", func(ctx *gin.Context) {
+			ctx.String(http.StatusOK, "user add")
+		})
+		user.DELETE("/delete", func(ctx *gin.Context) {
+			ctx.String(http.StatusOK, "user delete")
+		})
+	}
+
+	return r
+}
